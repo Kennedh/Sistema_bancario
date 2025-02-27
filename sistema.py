f43b15096b8a2d6f9559d77bd80019b99e03caaf
kennedh <ken.cmarcellino@gmail.com>
2025-02-27 19:15:27 -0300

    Update sistema

--- sistema.py
+++ sistema.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 menu = """
 
 [d] Depositar
@@ -10,14 +12,17 @@ menu = """
 saldo = 0
 limite = 500
 extrato = ""
-numero = 0
-LIMITE_SAQUES = 3
+LIMITE_TRANSACOES = 10
 
 def deposito():
     global saldo
     global extrato
+    global LIMITE_TRANSACOES
 
     while True:
+        if LIMITE_TRANSACOES == 10:
+            print("Não foi possivel realizar a transação. Limite de transações excedido.")
+            break
         deposit = float(input("Insira o valor que você deseja depositar: "))
         if deposit < 0:
             print("Valor inválido, digite o valor correto")
@@ -26,17 +31,20 @@ def deposito():
         else:
             saldo += deposit
             print("Deposito concluido!")
-            extrato += f"\nDeposito: R$ {deposit:.2f}"
+            extrato += f"\nDeposito: R$ {deposit:.2f} Data: {datetime.now().replace(microsecond=0)}"
+            LIMITE_TRANSACOES += 1
             break
 
 def saque():
     global saldo
     global limite
-    global numero
-    global LIMITE_SAQUES
+    global LIMITE_TRANSACOES
     global extrato
 
     while True:
+        if LIMITE_TRANSACOES == 10:
+            print("Não foi possivel realizar a transação. Limite de transações excedido.")
+            break
         if saldo == 0:
             print("Você não possui saldo, deposite algum valor para poder sacar.")
             break
@@ -45,20 +53,18 @@ def saque():
             print(f"Saldo insuficiente\n\nSaldo Atual: {saldo}")
         elif valor > limite:
             print("Valor maior que limite permitido por saque (R$ 500)")
-        elif numero >= LIMITE_SAQUES:
-            print("Limite de saque diário excedido. Tente novamente amanhã.")
             break
         elif valor < float(0):
             print("Valor não permitido")
         else:
             saldo -= valor
             print("Saque efetuado com sucesso!")
-            extrato += f"\nSaque: R$ {valor:.2f}"
-            numero += 1
+            extrato += f"\nSaque: R$ {valor:.2f} Data: {datetime.now().replace(microsecond=0)}"
+            LIMITE_TRANSACOES += 1
             break
 
 def consulta_extrato():
-    print(f"{extrato}\n\nSaldo atual: R$ {saldo:.2f}\n{numero}")
+    print(f"{extrato}\n\nSaldo atual: R$ {saldo:.2f}")
 
 while True:
     opcao = input(menu)
